983d7dcdccec98a138f30e4fa4c8fbeb8b30112d
Ahmed-Raza4 <hafizahmedraza12345@gmail.com>
2025-10-03 18:25:04 +0500

    minor-fixes

--- script.js
+++ script.js
@@ -4,23 +4,18 @@ let incrementBtn = document.getElementById('increment-btn');
 let decrementBtn = document.getElementById('decrement-btn');
 let resetBtn = document.getElementById('reset-btn');
 
-// set initial count 
 let count = 0;
-
-// function to update the display
 
 function updateDisplay() {
     countValue.textContent = count;
 }
 updateDisplay();
 
-// increment function
 incrementBtn.addEventListener('click', function(){
     count++;
     updateDisplay();
 })
 
-// decrement function
 decrementBtn.addEventListener('click', function(){
     if(count > 0){
         count--;
@@ -30,7 +25,6 @@ decrementBtn.addEventListener('click', function(){
     }
 })
 
-// reset function
 resetBtn.addEventListener('click', function(){
     count = 0;
     updateDisplay();
